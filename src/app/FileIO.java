057cf3677d2c8a114159f0d126108e2ba8caa358
Cong MA <macong1989@gmail.com>
2017-06-12 22:56:12 +1000

    Implement and test readFromFile method

--- src/app/FileIO.java
+++ src/app/FileIO.java
@@ -1,5 +1,12 @@
 package app;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -11,7 +18,19 @@ public class FileIO {
     }
 
     public static List<String> readFromFile(String path) {
-        List<String> res = null;
+        List<String> res = new ArrayList<>();
+        try {
+            File f = new File(path);
+            BufferedReader b = new BufferedReader(new FileReader(f));
+            String readLine = "";
+            System.out.println("Reading file using Buffered Reader");
+            while ((readLine = b.readLine()) != null) {
+                res.add(readLine);
+            }
+            b.close();
+        } catch (IOException e) {
+            System.out.println("Read file: " + path + " failed");
+        }
         return res;
     }
 }
--- src/test/FileIOTest.java
+++ src/test/FileIOTest.java
@@ -1,8 +1,15 @@
 package test;
 
+import app.FileIO;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -10,8 +17,20 @@ import static org.junit.jupiter.api.Assertions.*;
  * Created by admin on 12/06/2017.
  */
 class FileIOTest {
+    private String testString = "firstname lastname";
+    private Path readFile;
+
     @BeforeEach
     void setUp() {
+        readFile = Paths.get("./unsorted-names-list.txt");
+        testString = "firstname lastname";
+
+        byte[] writeContent = testString.getBytes();
+        try {
+            Files.write(readFile, writeContent);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     @AfterEach
@@ -24,6 +43,13 @@ class FileIOTest {
 
     @Test
     void readFromFile() {
+        List<String> readContent = FileIO.readFromFile("./unsorted-names-list.txt");
+        assertEquals(testString, readContent.get(0));
     }
 
+    @Test
+    void readFromFileWithIncorrectPath() {
+        List<String> readContent = FileIO.readFromFile("");
+        assertTrue(readContent.isEmpty());
+    }
 }
