86df27ff4c7ce85b17a2294c4e414e29bd627ffb
Cong MA <macong1989@gmail.com>
2017-06-12 23:21:51 +1000

    Implement and test parseName method

--- src/app/Name.java
+++ src/app/Name.java
@@ -4,7 +4,7 @@ package app;
  * Created by admin on 12/06/2017.
  */
 public class Name {
-    private String[] givenName;
+    private String[] givenName = new String[3];
     private String lastName;
     public String[] getGivenName() {
         return givenName;
@@ -20,5 +20,13 @@ public class Name {
 
     public void setLastName(String lastName) {
         this.lastName = lastName;
+    }
+
+    public void parseName(String str) {
+        String[] splitStr = str.split("\\s+");
+        this.lastName = splitStr[splitStr.length - 1];
+        for(int i = 0; i < (splitStr.length - 1); i++) {
+            this.givenName[i] = splitStr[i];
+        }
     }
 }
---
+++ src/test/NameTest.java
@@ -0,0 +1,25 @@
+package test;
+
+import app.Name;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * Created by admin on 12/06/2017.
+ */
+class NameTest {
+    @Test
+    void parseName() {
+        String testString = "Cong MA";
+        Name name = new Name();
+        name.parseName(testString);
+        String[] expectedGivenName = new String[3];
+        expectedGivenName[0] = "Cong";
+        String expectedLastName = "MA";
+        
+        assertArrayEquals(expectedGivenName, name.getGivenName());
+        assertEquals(expectedLastName, name.getLastName());
+    }
+
+}
