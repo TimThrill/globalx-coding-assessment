a385e39486c20a39583581e91525abf00f47c12e
Cong MA <macong1989@gmail.com>
2017-06-13 09:40:22 +1000

    Implement and test writeToFile method

--- src/app/FileIO.java
+++ src/app/FileIO.java
@@ -1,9 +1,6 @@
 package app;
 
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileReader;
-import java.io.IOException;
+import java.io.*;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
@@ -14,22 +11,43 @@ import java.util.List;
  */
 public class FileIO {
     public static void writeToFile(String path, List<Name> names) {
-
+        try {
+            File f = new File(path);
+            BufferedWriter bw = new BufferedWriter(new FileWriter(f));
+            for(int i = 0; i < names.size(); i++) {
+                String[] givenName = names.get(i).getGivenName();
+                for(int j = 0; j < givenName.length; j++) {
+                    if((givenName[j] != null) && !givenName[j].isEmpty()) {
+                        bw.write(givenName[j] + " ");
+                    }
+                }
+                bw.write(names.get(i).getLastName() + "\n");
+            }
+            bw.close();
+        } catch (IOException e) {
+            System.out.println("Write file: " + path + " failed");
+            System.out.println(e.getStackTrace());
+        }
     }
 
+    /*
+     * The method read file from a specific file line by line and return a List of those lines
+     * @param path The path of file to read
+     * @return A list of line contents in the file
+     */
     public static List<String> readFromFile(String path) {
         List<String> res = new ArrayList<>();
         try {
             File f = new File(path);
             BufferedReader b = new BufferedReader(new FileReader(f));
             String readLine = "";
-            System.out.println("Reading file using Buffered Reader");
             while ((readLine = b.readLine()) != null) {
                 res.add(readLine);
             }
             b.close();
         } catch (IOException e) {
             System.out.println("Read file: " + path + " failed");
+            System.out.println(e.getStackTrace());
         }
         return res;
     }
--- src/test/FileIOTest.java
+++ src/test/FileIOTest.java
@@ -1,14 +1,19 @@
 package test;
 
 import app.FileIO;
+import app.Name;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.ArrayList;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -22,7 +27,7 @@ class FileIOTest {
 
     @BeforeEach
     void setUp() {
-        readFile = Paths.get("./unsorted-names-list.txt");
+        readFile = Paths.get("./test-unsorted-names-list.txt");
         testString = "firstname lastname";
 
         byte[] writeContent = testString.getBytes();
@@ -35,15 +40,49 @@ class FileIOTest {
 
     @AfterEach
     void tearDown() {
+        try {
+            Files.delete(readFile);
+        } catch (Exception e) {
+            System.out.println("Delete test file failed");
+            System.out.println(e.getStackTrace());
+        }
     }
 
     @Test
     void writeToFile() {
+        String writeFilePath = "./test-sorted-names-list.txt";
+        String name1 = "Cong MA";
+        String name2 = "Cong Fei Lao";
+        List<String> expectedNames = new ArrayList<>();
+        expectedNames.add(name1);
+        expectedNames.add(name2);
+
+        List<Name> names = new ArrayList<>();
+        names.add(new Name());
+        names.get(0).parseName(name1);
+        names.add(new Name());
+        names.get(1).parseName(name2);
+
+        FileIO.writeToFile(writeFilePath, names);
+
+        try {
+            File f = new File(writeFilePath);
+            BufferedReader b = new BufferedReader(new FileReader(f));
+            String readLine = "";
+            int cnt = 0;
+            while ((readLine = b.readLine()) != null) {
+                assertEquals(expectedNames.get(cnt), readLine);
+                cnt++;
+            }
+            b.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     @Test
     void readFromFile() {
-        List<String> readContent = FileIO.readFromFile("./unsorted-names-list.txt");
+        List<String> readContent = FileIO.readFromFile("./test-unsorted-names-list.txt");
         assertEquals(testString, readContent.get(0));
     }
 
